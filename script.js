cd2f944a056a4760dfb462522d674fc508a811bf
Luciana Romero <lucianaromero0512@gmail.com>
2021-11-12 14:02:26 -0300

    agregue secciones prox js

---
+++ script.js
@@ -0,0 +1,28 @@
+/*const chageToImage = () => {
+    let sectionImagen = document.getElementById('section-imagen')
+    sectionImagen.classList.remove('oculto')
+
+    let aside = document.getElementById('aside')
+    console.log(aside)
+    aside.style.display='block'
+
+    let sectionTexto = document.getElementById('section-texto')
+    sectionTexto.classList.add('oculto')
+}
+
+imageToggle.addEventListener('click', chageToImage)
+
+const changeToText = () => {
+
+    let sectionTexto = document.getElementById('section-texto')
+    sectionTexto.classList.remove('oculto')
+    
+    let aside = document.getElementById('aside')
+    aside.style.display='block'
+
+    let sectionImagen = document.getElementById('section-imagen')
+    sectionImagen.classList.add('oculto')
+}
+textAsideToggle.addEventListener('click', changeToText) */
+
+//
